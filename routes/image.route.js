913708e69447d954d5a12bfd998572c428e3f338
duythanh2k <thanhphamduy2k@gmail.com>
2022-03-02 16:14:00 +0700

    setup base

---
+++ routes/image.route.js
@@ -0,0 +1,8 @@
+// module.exports = app => {
+//     var router  = require('express').Router();
+//     const posts = require('../controllers/image.controller');
+
+
+    
+//     app.use('/images', router);
+// }
