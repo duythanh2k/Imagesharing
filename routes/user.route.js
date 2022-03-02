03a135a1494ab34d4c91d3bdbcbfb4bbd6e9554a
duythanh2k <thanhphamduy2k@gmail.com>
2022-03-02 17:56:13 +0700

    update setup base

---
+++ routes/user.route.js
@@ -0,0 +1,8 @@
+// module.exports = app => {
+//     var router  = require('express').Router();
+//     const users = require('../controllers/user.controller');
+
+//     //router.get("/", users.getAllUsers);
+
+//     app.use('/users', router);
+// }
